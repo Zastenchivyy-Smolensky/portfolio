71745d0e73900e90067750c87730cbd2cb6e2420
Zastenchivyy-Smolensky <s20c2007@bene.fit.ac.jp>
2022-03-04 23:20:18 +0900

    finish create-detail-delete

--- backend/app/controllers/api/v1/products_controller.rb
+++ backend/app/controllers/api/v1/products_controller.rb
@@ -1,20 +1,20 @@
 class Api::V1::ProductsController < ApplicationController
     def index
         products = Product.all
-        render json: { status: 'SUCCESS', message: 'Loaded products', data: products }
+        render json: products
     end
 
     def show
         @product = Product.find(params[:id])
-        render json: {status: "ok",message:"Load product", data:@product}
+        render json:@product
     end
 
     def create
         product = Product.new(product_params)
         if product.save
-            render json: {status: 200, data:product}
+            render json: product
         else
-            render json: {status: 500, data: product.errors}
+            render json: product.errors
         end
     end
     
@@ -28,9 +28,9 @@ class Api::V1::ProductsController < ApplicationController
     def update
         @product = Product.find(params[:id])
         if @product.update(product_params)
-            render json: {status: "SUCCESS",message:"成功", data: @product}
+            render json: @product
         else
-            render json: { status: 'SUCCESS', message: 'Not updated', data: @product.errors }
+            render json: @product.errors
         end
     end
     
